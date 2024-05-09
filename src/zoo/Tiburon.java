f24dc32e95347202001d3dd3c607bfd817c735fe
JPLY-XYZ <josecabeza.compras@gmail.com>
2024-05-09 19:45:23 +0200

    Generando y añadiendo javadoc

--- src/zoo/Tiburon.java
+++ src/zoo/Tiburon.java
@@ -1,11 +1,19 @@
 package zoo;
 
-
+/**
+ *
+ * @author JPLY-XYZ - Jose Cabeza Reinoso
+ * 
+ * Clase tiburon, esta clase es el animal tiburon extiende de la clase pez.
+ */
 public class Tiburon extends Pez {
     private String especie;
     private boolean peligroso;
     private static int numTiburones = 0;
 
+    /**
+     *  Constructor por defecto de tiburon, no tiene parametros y es usado para definir los parametros de la clase padre
+     */
     public Tiburon () {
         super(20, Tipo.CARTILAGINOSO, Animal.Habitat.ARTICO, "focas", 100);
         this.especie = "Tiburón blanco";
@@ -13,6 +21,13 @@ public class Tiburon extends Pez {
         this.numTiburones ++;
     }
     
+    /**
+     *  Constructor parametrizado de la clase tiburon.
+     * @param especie Parametro de texto String , Se debe indicar de que especie es.
+     * @param peligroso Parametro tipo boleano, Indica si el animal es peligroso
+     * @param velocidadMaxNatacion Parametro tipo int, Indica la velocidad maxima a la que es capaz de nadar
+     * @param tipo Parametro tipo Tipo, deberas pasarle un objeto Tipo para indicar su tipo
+     */
     public Tiburon(String especie, boolean peligroso, int velocidadMaxNatacion, Tipo tipo) {
         super(velocidadMaxNatacion, tipo);
         this.especie = especie;
@@ -20,6 +35,16 @@ public class Tiburon extends Pez {
         this.numTiburones ++;
     }
 
+    /**
+     *
+     * @param especie Parametro de texto String , Se debe indicar de que especie es.
+     * @param peligroso Parametro tipo boleano, Indica si el animal es peligroso
+     * @param velocidadMaxNatacion Parametro tipo int, Indica la velocidad maxima a la que es capaz de nadar
+     * @param tipo Parametro tipo Tipo, deberas pasarle un objeto Tipo para indicar su tipo
+     * @param habitat PArametro tipo Habitat, deberas pasarle un objeto Habitat para indicar su habitad
+     * @param comida Parametro de texto String , Se debe indicar de que se alimenta.
+     * @param longevidad Parametro tipo int, Indica la cantidad de años que es capaz de vivir el animal
+     */
     public Tiburon(String especie, boolean peligroso, int velocidadMaxNatacion, Tipo tipo, Habitat habitat, String comida, int longevidad) {
         super(velocidadMaxNatacion, tipo, habitat, comida, longevidad);
         this.especie = especie;
@@ -27,14 +52,24 @@ public class Tiburon extends Pez {
         this.numTiburones ++;
     }
     
+    /**
+     *  Es un metodo que devuelve por pantalla un texto indicando la velocidad maxima que es capaz de alcanzar
+     */
     public void desplazar() {
         System.out.println("Estoy nadando a " + this.getVelocidadMaxNatacion() + " km/h.");
     }
     
+    /**
+     *  Es un metodo que devuelve la comida que come
+     */
     public void comer() {
         System.out.println("Estoy comiendo " + this.comida);    
     }
     
+    /**
+     *  Metodo protected el cual llama un metodo de la clase padre, el cual finaliza el tiburon
+     * @throws Throwable
+     */
     @Override
     protected void finalize() throws Throwable {
         numTiburones--;
